de5a9ef1ea0515dd29b8055c33e89a6f2c0a1834
birisi atuya <tushhamphrey@gamil.com>
2024-06-05 14:33:39 +0300

    build home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import HeroSecLayout from "../components/herosection/HeroSecLayout";
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <HeroSecLayout />
+      <Footer />
+    </>
+  );
+}
