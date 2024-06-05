830aed22d268e34184dd62ea6396933ebb48fbf7
birisi atuya <tushhamphrey@gamil.com>
2024-06-05 14:20:33 +0300

    build footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import HeroSecLayout from "./components/herosection/HeroSecLayout";
 
@@ -6,6 +7,7 @@ export default function App() {
     <>
       <Navbar />
       <HeroSecLayout />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-center py-3 text-white bg-background">
+      <small>copyright@2024</small>
+    </div>
+  );
+}
