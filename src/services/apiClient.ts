f2740d021f47d1e775b2728305551adfb2e50bb1
birisi atuya <tushhamphrey@gamil.com>
2024-06-05 19:18:10 +0300

    fix posts endpoint

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-const endPoint = import.meta.env.VITE_ENDPOINT
 export default axios.create({
-    baseURL: endPoint,
+    baseURL: "https://jsonplaceholder.typicode.com",
 });
